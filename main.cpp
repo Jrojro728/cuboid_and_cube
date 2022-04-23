47e4b0530e9c9662a0c7a1303edc8c79414acaba
Jrojro728 <3101109252@qq.com>
2022-04-23 17:37:48 +0800

    test

--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@
 #include "define.h"
 #include "cuboid.cpp"
 
+cuboid *cubo = new cuboid();
+
 int main()
 {
+	double a{};
+	double b{};
+	double h{};
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >>	h;
+	cubo->Rectangle3in1(a, b, h);
+	std::cout << cubo->getCuboidSize();
+
 	system("pause");
 
 	return 0;
