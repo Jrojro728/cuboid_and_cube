13b3d9ff77f7fc6bcebd9ef6f20e23537e594c39
Jrojro728 <3101109252@qq.com>
2022-04-24 17:58:46 +0800

    完成基本功能

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ cuboid *cubo = new cuboid();
 
 int main()
 {
+	uint8_t choice{};
 	long double a{};
 	long double b{};
 	long double h{};
@@ -20,7 +21,17 @@ int main()
 	std::cout << "请再输入另一个数字:";
 	std::cin >>	h;
 	cubo->Rectangle3in1(a, b, h);
-	std::cout << cubo->getCuboidSize() << "\n";
+	std::cout << "请输入你的选择:\n"
+			  << "(1):求面积 (2):求体积\n";
+	std::cin >> choice;
+	if (choice == 1)
+	{
+		std::cout << "面积:" << cubo->getCuboidArea();
+	}
+	else 
+	{
+		std::cout << "体积:" << cubo->getCuboidSize();
+	}
 
 	system("pause");
 
